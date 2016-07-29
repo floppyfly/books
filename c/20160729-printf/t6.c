955fb7be27728e5d17b3c95dc0a7702182116220
floppyfly <floppyfly@outlook.com>
2016-07-29 12:25:48 +0800

    add 7 files for testing printf

---
+++ c/20160729-printf/t6.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void){
+	printf("+----------------+\n");
+	printf("|Hello,floppyfly!|\n");
+	printf("+----------------+\n");
+	return 0;
+}
---
+++ c/20160729-printf/t7.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void){
+	printf("+========================================+\n");
+	printf("|Hello floppyfly[batinhu]andbeautyandjack|\n");
+	printf("+========================================+\n");
+	return 0;
+}

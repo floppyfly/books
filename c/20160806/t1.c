affe863edd680bbfceb05a333b4f35bf63c603e9
floppyfly <floppyfly@outlook.com>
2016-08-08 21:32:24 +0800

    字符和整型变量的互换

---
+++ c/20160806/t1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void){
+	char a;
+	a='a';
+	printf("%c=%d\n",a,a);
+	a='A';
+	printf("%c=%d\n",a,a);
+	a='b';
+	printf("%c=%d\n",a,a);
+	a='B';
+	printf("%c=%d\n",a,a);
+	return 0;
+}
---
+++ c/20160806/t2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void){
+	int a;
+	a=65;
+	a=a+260;
+	printf("a=%c\n",a);
+	return 0;
+}
